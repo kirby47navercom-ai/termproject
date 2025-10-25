b36a38c5358d7763a188fe18f35a2be9d2f2ad6c
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-25 19:20:18 +0900

    반복문에 업데이트 다음 랜더 넣기

--- game_framework.py
+++ game_framework.py
@@ -13,6 +13,8 @@ def ingame(game_manager):
     frame_time = 0.0
     current_time = time.time()
     while running:
+        game_manager.update(frame_time)
+        game_manager.render()
         frame_time = time.time() - current_time
         current_time += frame_time
         pass
