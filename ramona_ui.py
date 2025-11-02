ac0178eee1c53f7943938afd62d414ef46782a00
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:45:30 +0900

    플레이어 ui 뼈대 완성

---
+++ ramona_ui.py
@@ -0,0 +1,16 @@
+from pico2d import *
+from canvas_size import *
+import ramona
+
+class Ramona_UI:
+    def __init__(self):
+
+        pass
+
+    def update(self, frame_time, events=None):
+
+        pass
+
+    def draw(self):
+                     50, 50)
+        pass
