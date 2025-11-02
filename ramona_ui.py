583f0f6cb406fa820070a89b5fbd2decd04268bb
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:45:50 +0900

    일단 체력쪽만 넣어주기

--- ramona_ui.py
+++ ramona_ui.py
@@ -4,7 +4,8 @@ import ramona
 
 class Ramona_UI:
     def __init__(self):
-
+        self.ramona_hp_image = load_image('playerui\\heart1.png')
+        self.ramona_broken_hp_image = load_image('playerui\\heart2.png')
         pass
 
     def update(self, frame_time, events=None):
@@ -12,5 +13,13 @@ class Ramona_UI:
         pass
 
     def draw(self):
-                     50, 50)
+        for i in range(ramona.MAX_HP):
+            if i < ramona.MAX_HP - ramona.CURRENT_HP:
+                self.ramona_broken_hp_image.clip_draw(0, 0, 17, 17, (50 + (ramona.MAX_HP - 1) * 70) - i * 70,
+                                                      canvasheight - 50,
+                                                      50, 50)
+            else:
+                self.ramona_hp_image.clip_draw(0, 0, 17, 17, (50 + (ramona.MAX_HP - 1) * 70) - i * 70,
+                                               canvasheight - 50,
+                                               50, 50)
         pass
