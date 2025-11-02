414b276d8dba66669ca7e3dfc4c24312e3ced882
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:59:12 +0900

    물리변수 만들어주기

---
+++ ramona.py
@@ -0,0 +1,11 @@
+from pico2d import *
+import time
+import resource
+import canvas_size
+import draw_gesture
+import random
+
+# 물리
+GRAVITY = 2000.0
+GROUND_LEVEL = 100
+WIDTH_LEVEL = canvas_size.canvaswidth-25
