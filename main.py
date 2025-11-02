6f30838e781c145361d587d79caa92390a668e14
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 13:56:32 +0900

    일단 모드를 스타트 모드로 변경하고 메인으로 시작하게 하기

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pico2d
+import game_framework
+import game_manager as start_mode
+import resource
+from canvas_size import *
 
+pico2d.open_canvas(canvaswidth, canvasheight)
+pico2d.hide_lattice()
+pico2d.hide_cursor()
+resource.load_resources()
+game_framework.ingame(start_mode)
+pico2d.close_canvas()
 
 #2022180021 양현빈
