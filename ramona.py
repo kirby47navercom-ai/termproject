8234ec5cfeda1f3f8a0c6f88a1f5d1e1ad694f56
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:59:20 +0900

    속도 변수 만들어주기

--- ramona.py
+++ ramona.py
@@ -9,3 +9,9 @@ import random
 GRAVITY = 2000.0
 GROUND_LEVEL = 100
 WIDTH_LEVEL = canvas_size.canvaswidth-25
+# 속도 (pixel/sec)
+WALK_SPEED = 200.0
+RUN_SPEED = 350.0
+EVADE_SPEED = 500.0
+JUMP_POWER = 600.0
+DEATH_KNOCKBACK_SPEED = 150.0
