b027c4af92df29fe442f57ae64d6d1ded83b2ea8
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:45:08 +0900

    플레이어 ui 설정

---
+++ draw_gesture.py
@@ -0,0 +1,9 @@
+from pico2d import *
+import os
+from QDollarRecognizer import QDollarRecognizer, Point
+import canvas_size
+#import ramona
+
+check_image_width = 825
+check_image_height = 216
+SIZE=20
