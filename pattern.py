685576a75078712b1a20d067ba21f78ed255601f
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:48:27 +0900

    패턴 클래스 뼈대 완성

---
+++ pattern.py
@@ -0,0 +1,12 @@
+from pico2d import *
+
+import canvas_size
+from canvas_size import *
+from resource import *
+
+class Pattern:
+    def __init__(self):
+        pass
+
+    def draw(self,sizex=0.2,sizey=0.2):
+        pass
