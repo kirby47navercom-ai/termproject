45974703c414d8c405d6f43aa35def7720eb1589
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:48:50 +0900

    가로선 클래스 완성

--- pattern.py
+++ pattern.py
@@ -16,3 +16,8 @@ class Pattern:
     def draw(self,sizex=0.2,sizey=0.2):
         self.image.clip_draw(0, 0, 128, 128,self.x-canvas_size.camera_x, self.y-canvas_size.camera_y, self.width*sizex, self.height*sizey)
 
+class Width(Pattern):
+    def __init__(self):
+        super().__init__()
+        self.image = image_pattern[0]
+        self.name = '가로선'
