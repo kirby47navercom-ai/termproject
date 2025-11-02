38867f99f491d2a171a9df79ad57dbbb52055efe
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:48:38 +0900

    패턴 클래스 완성

--- pattern.py
+++ pattern.py
@@ -6,7 +6,13 @@ from resource import *
 
 class Pattern:
     def __init__(self):
-        pass
+        self.width = 128
+        self.height = 128
+        self.x = None
+        self.y = None
+        self.image = None
+        self.name = None
 
     def draw(self,sizex=0.2,sizey=0.2):
-        pass
+        self.image.clip_draw(0, 0, 128, 128,self.x-canvas_size.camera_x, self.y-canvas_size.camera_y, self.width*sizex, self.height*sizey)
+
