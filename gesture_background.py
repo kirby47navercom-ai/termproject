8fd0cc677172de6d92d3f927ace1eba6dd007647
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:03:44 +0900

    제스쳐 백그라운드 뼈대완성

--- gesture_background.py
+++ gesture_background.py
@@ -4,3 +4,21 @@ from canvas_size import *
 check_image_width = 825
 check_image_height = 216
 SIZE=20
+
+class GestureBackground:
+    def __init__(self):
+
+        pass
+
+    def update(self, frame_time, events):
+
+        pass
+
+
+    def handle_event(self, events):
+        pass
+
+
+    def draw(self):
+
+        pass
