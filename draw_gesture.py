c084d7ccaf3d39a7b7d629bbb437113dbe845526
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 16:58:06 +0900

    f 키 만들기

--- draw_gesture.py
+++ draw_gesture.py
@@ -2,8 +2,14 @@ from pico2d import *
 import os
 from QDollarRecognizer import QDollarRecognizer, Point
 import canvas_size
-#import ramona
+import ramona
 
 check_image_width = 825
 check_image_height = 216
 SIZE=20
+
+BLACK = (0, 0, 0)
+
+f_pressed = True
+
+result = None
