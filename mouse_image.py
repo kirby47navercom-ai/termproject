69b0f82b6dd200bda67b1cd1946181596c9e9edd
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-02 14:08:45 +0900

    마우스 클래스 뼈대 만들기

--- mouse_image.py
+++ mouse_image.py
@@ -1,3 +1,16 @@
 from pico2d import *
 #import draw_gesture
 width, height = 128, 128
+
+class Mouse:
+    def __init__(self):
+
+        pass
+
+    def update(self, frame_time,events):
+
+        pass
+
+    def draw(self):
+
+        pass
