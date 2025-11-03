702e5704154181e7809bd24bf7e4ffcf90fce9a2
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-03 19:40:26 +0900

    유령 다 만듬

--- ghost_normal.py
+++ ghost_normal.py
@@ -32,8 +32,56 @@ class Ghost:
         self.hit_animation = False
         self.hit_animation_speed = 8.0
         self.hit_frame = 0
+
     def update(self, frame_time, events=None):
-        pass
+
+        if not self.die_animation and not self.hit_animation:
+            self.move(frame_time)
+        elif self.hit_animation and not self.die_animation:
+            self.hit_ghost_animation(frame_time)
+        elif self.die_animation:
+            self.die_ghost_animation(frame_time)
+
+        self.ramonatoghost()
+        self.die_ghost()
+
+    def move(self, frame_time):
+        distance = math.sqrt((self.x - ramona.Ramona_POS_X) ** 2 + (self.y - ramona.Ramona_POS_Y) ** 2)
+        self.x = self.x + (ramona.Ramona_POS_X - self.x) * self.speed * frame_time / distance
+        self.y = self.y + (ramona.Ramona_POS_Y - self.y) * self.speed * frame_time / distance
+
+        self.shape.x = self.x
+        self.shape.y = self.y + self.height * 0.7
+
+    def ramonatoghost(self):
+        if collide([ramona.Ramona_POS_X, ramona.Ramona_POS_Y, ramona.Ramona_SIZE_X, ramona.Ramona_SIZE_Y],
+                   [self.x, self.y, self.width,
+                    self.height]) and not ramona.Ramona_invincible and not ramona.Ramona_roll_invincible and not self.die_animation:
+            if ramona.CURRENT_HP > 0:
+                ramona.CURRENT_HP -= 1
+                ramona.Ramona_invincible = True
+                ramona.invincible_timer = 0.0
+                self.die_animation = True
+                canvas_size.start_shake(0.5, 5.0)
+
+
+    def hit_ghost_animation(self, frame_time):
+        self.hit_frame = (self.hit_frame + self.hit_animation_speed * frame_time) % 4
+        if int(self.hit_frame) == 3:
+            self.hit_animation = False
+            self.shape = self.pattern_set[randint(0, pattern_number - 5)]
+            self.shape.x = self.x
+
+    def die_ghost(self):
+        if self.hp <= 0 and not self.die_animation:
+            self.die_animation = True
+            self.shape.name = 'No'
+
+    def die_ghost_animation(self, frame_time):
+        self.die_frame = (self.die_frame + self.die_animation_speed * frame_time) % 4
+        if int(self.die_frame) == 3:
+            self.die = True
+
 
 
 
