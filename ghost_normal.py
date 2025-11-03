757e89f65798c4c115a40b0a166daa7e3eb45710
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-03 19:23:17 +0900

    몬스터 클래스 만듬

---
+++ ghost_normal.py
@@ -0,0 +1,20 @@
+from pico2d import *
+from pattern import *
+from resource import *
+from random import randint
+import canvas_size
+import ramona
+import math
+
+class Ghost:
+    image=None
+    def __init__(self):
+        pass
+    def update(self, frame_time, events=None):
+        pass
+
+
+
+    def draw(self):
+        pass
+
