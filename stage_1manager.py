ffdbde19ebfe6c9ca60fe2fe1aa4141e082cb18b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-03 14:45:44 +0900

    일단 init update draw 만듬

---
+++ stage_1manager.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import ramona
+
+import draw_gesture
+import ramona_ui
+
+import canvas_size
+
+def init():
+    pass
+
+
+def update(frame_time,events):
+    pass
+
+
+
+def draw():
+    pass
