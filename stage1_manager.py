d863968db31693f6f0d7a4e078ab011b9ac19967
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-03 19:27:32 +0900

    테스트를 위해 그리고 실행을 위한 파일 만들고 메니저 작성

---
+++ stage1_manager.py
@@ -0,0 +1,54 @@
+from pico2d import *
+import ramona
+import background_1stage
+import draw_gesture
+import ramona_ui
+import stage1_monster
+import canvas_size
+
+def init():
+    global player, stage_background, draw_gest, ramona_ui_, stage1_monster_
+    stage_background = background_1stage.Background()
+    player = ramona.Ramona()
+    ramona_ui_ = ramona_ui.Ramona_UI()
+    stage1_monster_ = stage1_monster.Stage1_Monster()
+    draw_gest = draw_gesture.GestureRecognizer()
+
+    ramona.GROUND_LEVEL = -100
+    ramona.WIDTH_LEVEL = canvas_size.canvaswidth - 25
+
+    player.x = canvas_size.canvaswidth // 2
+    player.y = ramona.GROUND_LEVEL
+
+    canvas_size.camera_x = 0
+    canvas_size.camera_y = 0
+
+
+def update(frame_time,events):
+    global player, stage_background, draw_gest, ramona_ui_, stage1_monster_
+    if not ramona.Ramona_dead:
+        stage_background.update(frame_time, events)
+        ramona_ui_.update(frame_time, events)
+        stage1_monster_.update(frame_time, events)
+        draw_gest.update(frame_time, events)
+    else:
+        for event in events:
+            if event.type == SDL_KEYDOWN and event.key == SDLK_r:
+                ramona.Ramona_dead = False
+                ramona.CURRENT_HP = ramona.MAX_HP
+                ramona.Ramona_POS_X = 100
+                ramona.Ramona_POS_Y = ramona.GROUND_LEVEL
+                init()
+    player.update(frame_time, events)
+
+
+
+
+
+def draw():
+    global player, stage_background, draw_gest, ramona_ui_, stage1_monster_
+    stage_background.draw()
+    player.draw()
+    stage1_monster_.draw()
+    ramona_ui_.draw()
+    draw_gest.draw()
---
+++ stage1_monster.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+class Stage1_Monster:
+    def __init__(self):
+        pass
+
+    def update(self, frame_time, events=None):
+        pass
+
+    def draw(self):
+        pass
