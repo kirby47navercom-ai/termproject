8d05636ab1052b5a4b10cc8f7d54298c37fda312
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-03 14:47:55 +0900

    배경 클래스와 안에 함수만 정의를 해둠

--- background_1stage.py
+++ background_1stage.py
@@ -3,3 +3,15 @@ import canvas_size
 
 stage1width = 1980
 stage1height = 1080
+
+start=False
+
+class Background:
+    def __init__(self):
+        pass
+
+    def update(self, frame_time, events=None):
+        pass
+
+    def draw(self):
+        pass
