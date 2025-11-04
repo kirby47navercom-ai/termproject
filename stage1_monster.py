fe6fc1eb53fbd22091987ef31a1a93a886f893fa
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-04 16:08:30 +0900

    몬스터에다가 페이즈 넘기게 만듬

--- stage1_monster.py
+++ stage1_monster.py
@@ -1,3 +1,8 @@
+import stage1_phase1
+import stage1_phase2
+import stage1_phase3
+import stage1_phase4
+import stage1_phase5
 
 
 
@@ -5,10 +10,23 @@
 
 class Stage1_Monster:
     def __init__(self):
-        pass
+        self.phase_num = 0
+        self.phase = []
+        self.phase.append(stage1_phase1.Stage1_Phase1())
+        self.phase.append(stage1_phase2.Stage1_Phase2())
+        self.phase.append(stage1_phase3.Stage1_Phase3())
+        self.phase.append(stage1_phase4.Stage1_Phase4())
+        self.phase.append(stage1_phase5.Stage1_Phase5())
+
 
     def update(self, frame_time, events=None):
-        pass
+        self.phase[self.phase_num].update(frame_time, events)
+
+        if self.phase_num == 4:
+            pass
+        elif self.phase[self.phase_num].phase.__len__() == 0:
+            self.phase_num += 1
+            self.update(frame_time, events)
 
     def draw(self):
-        pass
+        self.phase[self.phase_num].draw()
