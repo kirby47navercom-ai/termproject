c37a5f1a4fbafa205ac06fd460fdff27f88c20ec
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-04 16:02:43 +0900

    5개 파일에 페이지 클래스 만듬

---
+++ stage1_phase4.py
@@ -0,0 +1,12 @@
+from ghost_normal import *
+import draw_gesture
+
+
+class Stage1_Phase4:
+    def __init__(self):
+        pass
+    def update(self, frame_time, events=None):
+        pass
+
+    def draw(self):
+        pass
---
+++ stage1_phase5.py
@@ -0,0 +1,11 @@
+import draw_gesture
+
+
+class Stage1_Phase5:
+    def __init__(self):
+        pass
+    def update(self, frame_time, events=None):
+        pass
+
+    def draw(self):
+        pass
