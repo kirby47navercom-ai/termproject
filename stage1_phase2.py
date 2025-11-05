2c747c4c6af463ed831e8d368bec01d5d9da8981
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-05 19:48:18 +0900

    페이즈 2 만들기

--- stage1_phase2.py
+++ stage1_phase2.py
@@ -1,12 +1,48 @@
 from ghost_normal import *
 import draw_gesture
 
+ghost_phase_far=50
+ghost_phase_pos=[-1,1,-1,1,-1,1,-1,1]
 
 class Stage1_Phase2:
     def __init__(self):
-        pass
+        self.phase=[Ghost() for _ in range(ghost_phase_pos.__len__())]
+        for i in range(self.phase.__len__()):
+            self.phase[i].x=0 if ghost_phase_pos[i] == -1 else canvaswidth+ghost_phase_pos[i]*ghost_phase_far
+            self.phase[i].y=random.randint(0,canvasheight)
+            self.phase[i].hp = random.randint(20,30)
+
+        self.num = 2
+
+
     def update(self, frame_time, events=None):
-        pass
+
+        for i in range(self.num):
+            self.phase[i].update(frame_time,events)
+
+        self.shape_check()
+        self.monster_die()
+
+
+    def shape_check(self):
+        for i in range(self.num):
+            if self.phase[i].shape.name==draw_gesture.result:
+                self.phase[i].hp-=ramona.Ramona_attack
+                self.phase[i].hit_animation=True
+                self.phase[i].hit_frame = 0
+                ramona.Ramona_smash = True
+
+        draw_gesture.result=None
+
+
+    def monster_die(self):
+        for i in range(self.num - 1, -1, -1):
+            if self.phase[i].die:
+                self.phase.pop(i)
+                self.num -= 1
+
+        if self.num == 0 and self.phase.__len__()>0: self.num = 2
 
     def draw(self):
-        pass
+        for i in range(self.num):
+            self.phase[i].draw()
