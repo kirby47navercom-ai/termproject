b1aa82fffcadce1775eb87b1bf528c73c5371702
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-06 16:27:58 +0900

    보스 hp 클래스 만듬

---
+++ boss_hp.py
@@ -0,0 +1,17 @@
+import canvas_size
+from resource import *
+
+
+class Boss_HP:
+
+
+    def __init__(self):
+       pass
+
+
+    def update(self, frame_time, events=None):
+
+        pass
+
+    def draw(self,boss_now_hp=240, boss_max_hp=240):
+        pass
