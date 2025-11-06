8ae9bc01e00853f28c029a13243e792e507dfce2
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-06 16:08:43 +0900

    페이즈 4 몇개좀 고침

--- stage1_phase4.py
+++ stage1_phase4.py
@@ -21,7 +21,7 @@ class Stage1_Phase4:
 
         self.shape_check()
         self.monster_die()
-        pass
+
 
     def shape_check(self):
         if self.phase[self.phase.__len__()-1].shape.name==draw_gesture.result:
@@ -36,7 +36,6 @@ class Stage1_Phase4:
     def monster_die(self):
         if self.phase[self.phase.__len__()-1].die:
             self.phase.pop(self.phase.__len__()-1)
-        pass
+
     def draw(self):
         self.phase[self.phase.__len__()-1].draw()
-        pass
