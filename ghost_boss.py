2eaf28790c557e0905b77257df4984bc51e210a5
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-07 20:24:51 +0900

    CutsceneState 뼈대 만듬

--- ghost_boss.py
+++ ghost_boss.py
@@ -8,6 +8,20 @@ import ramona
 
 SIZE = 1.2
 
+class CutsceneState:
+    def enter(self, event):
+        pass
+
+    def exit(self, event):
+        pass
+
+    def do(self, frame_time):
+        pass
+
+    def draw(self):
+        pass
+
+
 class ReadyState:
     def enter(self, event):
         pass
