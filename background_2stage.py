9252a4a54bd53442a6c8449bc6017970781e3198
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-13 16:25:31 +0900

    일단 배경 사이즈 넣는 변수 만들기

--- background_2stage.py
+++ background_2stage.py
@@ -12,11 +12,13 @@ class Background:
             Background.background =  [load_image('2stage\\1.png'), load_image('2stage\\2.png'),
                                load_image('2stage\\3.png')]
 
-            if Background.floor1 == None:
-                Background.floor1 = load_image('2stage\\floor_1.png')
+        self.background_size = [(872, 479), (726, 574), (1280 * 1.5, 2048 * 1.5)]
 
-            if Background.floor2 == None:
-                Background.floor2 = load_image('2stage\\floor_2.png')
+        if Background.floor1 == None:
+            Background.floor1 = load_image('2stage\\floor_1.png')
+
+        if Background.floor2 == None:
+            Background.floor2 = load_image('2stage\\floor_2.png')
 
     def update(self, frame_time, events=None):
         pass
