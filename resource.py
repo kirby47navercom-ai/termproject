7c3e7072d96afdfb687512753acad15858879fe2
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-13 14:19:33 +0900

    빔 이미지 넣기

--- resource.py
+++ resource.py
@@ -298,6 +298,8 @@ def load_resources():
 
     for i in range(28):
         boss_kitty_attack_image.append(load_image(f'2stage\\attack_{str(i+1)}.png'))
+    for i in range(4):
+        boss_kitty_uibim_image.append(load_image(f'2stage\\lightAttack{str(i+1)}.png'))
 
 
 
