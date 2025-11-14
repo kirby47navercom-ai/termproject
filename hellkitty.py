0dbcad3886cdb530d6d0c26d76609db049879532
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-15 00:20:24 +0900

    이미지 넣음

--- hellkitty.py
+++ hellkitty.py
@@ -18,7 +18,17 @@ class Boss_Kitty:
     little_image = None
     die_image = None
     def __init__(self):
-        pass
+        self.pattern_set = get_pattern_set()
+        if Boss_Kitty.image == None:
+            Boss_Kitty.image = [load_image('2stage\\boss1.png'), load_image('2stage\\boss2.png')]
+        if Boss_Kitty.die_image == None:
+            Boss_Kitty.die_image = resource.boss_kitty_die_image
+        if Boss_Kitty.attack1_image == None:
+            Boss_Kitty.attack1_image = resource.boss_kitty_attack_image
+        if Boss_Kitty.attack2_image == None:
+            Boss_Kitty.attack2_image = resource.boss_kitty_uibim_image
+        if Boss_Kitty.little_image == None:
+            Boss_Kitty.little_image = load_image('2stage\\157.png')
 
     def change_state(self, new_state, event):
         pass
