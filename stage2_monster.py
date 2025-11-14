d524e47eff1fdd6f670efe077f4c605c908d04e9
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-15 00:21:27 +0900

    헬로키티 임포트하기

--- stage2_monster.py
+++ stage2_monster.py
@@ -1,12 +1,12 @@
 import draw_gesture
 import background_2stage
-
+import hellkitty
 import ramona
 
 
 class Stage2_Monster:
     def __init__(self):
-        pass
+
 
     def update(self, frame_time, events=None):
         pass
