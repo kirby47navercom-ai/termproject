223be9de031ea8cc564facd322c857a386ec014a
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-15 00:04:22 +0900

    테스트 하기위해 메니저 넣어두기

--- game_manager.py
+++ game_manager.py
@@ -1,21 +1,25 @@
 from pico2d import *
 import mouse_image
 import stage1_manager
+import stage2_manager
 
 def init():
     global mouse
-    stage1_manager.init()
+    #stage1_manager.init()
+    stage2_manager.init()
     mouse = mouse_image.Mouse()
 
 def update(frame_time):
     global mouse
     events = get_events()
-    stage1_manager.update(frame_time, events)
+    #stage1_manager.update(frame_time, events)
+    stage2_manager.update(frame_time, events)
     mouse.update(frame_time,events)
 
 def draw():
     global mouse
     clear_canvas()
-    stage1_manager.draw()
+    #stage1_manager.draw()
+    stage2_manager.draw()
     mouse.draw()
     update_canvas()
