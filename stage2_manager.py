cf15f384e18332a559ad059170e7024a84874009
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-14 19:51:52 +0900

    초기 변수 넣기

--- stage2_manager.py
+++ stage2_manager.py
@@ -9,6 +9,11 @@ import game_framework
 
 def init():
     global player,stage_background,draw_gest,ramona_ui_,stage2_monster_
+    stage_background = background_2stage.Background()
+    player = ramona.Ramona()
+    stage2_monster_ = stage2_monster.Stage2_Monster()
+    ramona_ui_ = ramona_ui.Ramona_UI()
+    draw_gest = draw_gesture.GestureRecognizer()
 
 
 def update(frame_time,events):
