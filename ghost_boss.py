4b85f15e652eb093939391ae70e8637e55d70151
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-14 21:37:08 +0900

    고스트 보스 HitState 콜라인더 체크 넣어주기

--- ghost_boss.py
+++ ghost_boss.py
@@ -216,6 +216,12 @@ class HitState:
         self.image.clip_composite_draw(left, bottom, width, height, 0, 'h', self.x + jx - canvas_size.shake_x,
                                        self.y + jy - canvas_size.shake_y, width * SIZE, height * SIZE)
 
+        if canvas_size.collide_check:
+            draw_rectangle(self.x - width * SIZE / 2,
+                           self.y - height * SIZE / 2,
+                           self.x + width * SIZE / 2,
+                           self.y + height * SIZE / 2)
+
 
 
 class DieState:
