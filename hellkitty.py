f185210a4d294f581fe2e54047f39662d2fab292
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-15 00:02:50 +0900

    여러가지 임포트 하기

---
+++ hellkitty.py
@@ -0,0 +1,9 @@
+import background_2stage
+from boss_hp import Boss_HP
+from pattern import *
+from resource import *
+from random import randint
+import canvas_size
+import ramona
+import resource
+import math
