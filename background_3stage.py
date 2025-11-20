34658ca80be0d8ccaa472967517bba764450f695
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-20 23:07:33 +0900

    이미지 파일 넣음

--- background_3stage.py
+++ background_3stage.py
@@ -7,6 +7,10 @@ start = False
 width = 1980
 
 class Background:
+    fox_platform = None
+    fox_background1 = None
+    fox_background2 = None
+    fox_background3 = None
     def __init__(self):
         pass
     def update(self, frame_time, events=None):
