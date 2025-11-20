e2b6fa03bd276d379174e0c705d237dda29acacb
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-20 23:40:40 +0900

    카메라 스피드 크기 넣어주기

--- stage3_topography.py
+++ stage3_topography.py
@@ -18,7 +18,17 @@ class Stage3_Terrain:
     flame_ball_image = None
     white_background_image = None
     def __init__(self):
-        pass
+        self.terrain_y = [randint(100, 400) for _ in range(5)]
+        self.terrain_x = [randint(100, background_3stage.width - 100) for _ in range(5)]
+        self.terrain_width = 960
+        self.terrain_height = 128
+        self.pattern = [1, 2, 3]
+        random.shuffle(self.pattern)
+        self.current_pattern = 0
+        self.old_pattern = 0
+        self.speed = 15
+        self.camerax = canvas_size.camera_x
+        self.cameray = canvas_size.camera_y
     def update(self, frame_time, events=None):
         pass
     def draw(self):
