d12d0be2748c8a2d0e71d960779dd694f9cf7949
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-20 23:31:38 +0900

    stage3_manager 적용

--- game_manager.py
+++ game_manager.py
@@ -2,24 +2,28 @@ from pico2d import *
 import mouse_image
 import stage1_manager
 import stage2_manager
+import stage3_manager
 
 def init():
     global mouse
     #stage1_manager.init()
-    stage2_manager.init()
+    #stage2_manager.init()
+    stage3_manager.init()
     mouse = mouse_image.Mouse()
 
 def update(frame_time):
     global mouse
     events = get_events()
     #stage1_manager.update(frame_time, events)
-    stage2_manager.update(frame_time, events)
+    #stage2_manager.update(frame_time, events)
+    stage3_manager.update(frame_time, events)
     mouse.update(frame_time,events)
 
 def draw():
     global mouse
     clear_canvas()
     #stage1_manager.draw()
-    stage2_manager.draw()
+    #stage2_manager.draw()
+    stage3_manager.draw()
     mouse.draw()
     update_canvas()
