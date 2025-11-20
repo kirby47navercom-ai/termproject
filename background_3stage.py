45221203d09fc6232501a075bbf575abc2b8128b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-20 23:07:52 +0900

    플랫폼 사진과 크기 조정

--- background_3stage.py
+++ background_3stage.py
@@ -12,7 +12,11 @@ class Background:
     fox_background2 = None
     fox_background3 = None
     def __init__(self):
-        pass
+        resource.blocks.clear()
+        if Background.fox_platform == None:
+            Background.fox_platform = load_image('3stage\\Fox Platform.png')
+        self.fox_platform_size = (752, 128)
+
     def update(self, frame_time, events=None):
         pass
     def draw(self):
