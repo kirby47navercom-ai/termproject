937806fb77d6c19be197e737afdbc5e31c22fdfe
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-20 23:38:05 +0900

    이미지 변수 넣음

--- stage3_topography.py
+++ stage3_topography.py
@@ -9,6 +9,14 @@ import random
 width = 1980
 
 class Stage3_Terrain:
+    vine_image = None
+    water_image = None
+    flame_image = None
+    vine_needle_appear_image = None
+    vine_needle_disappear_image = None
+    water_wave_image = None
+    flame_ball_image = None
+    white_background_image = None
     def __init__(self):
         pass
     def update(self, frame_time, events=None):
