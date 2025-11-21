734bba34d0050f1561fbf5cb2eb371d1efe532f4
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-21 15:56:29 +0900

    매니저에다가 캐릭터 오브젝트 변수들 넣어주기

--- stage3_manager.py
+++ stage3_manager.py
@@ -5,9 +5,15 @@ import ramona_ui
 import canvas_size
 import game_framework
 import background_3stage
+import stage3_monster
 
 def init():
-    pass
+    global player, stage_background, draw_gest, ramona_ui_, stage3_monster_
+    stage_background = background_3stage.Background()
+    player = ramona.Ramona()
+    stage3_monster_ = stage3_monster.Stage3_Monster()
+    ramona_ui_ = ramona_ui.Ramona_UI()
+    draw_gest = draw_gesture.GestureRecognizer()
 
 
 def update(frame_time,events):
