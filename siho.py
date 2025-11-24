44fee659202552f7e9f2023ffe8a7df76cd545a2
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-24 22:28:00 +0900

    체력과 간단한거 설정

--- siho.py
+++ siho.py
@@ -12,7 +12,12 @@ SIZE = 1.8
 
 class Boss_Siho:
     def __init__(self):
-        pass
+        self.pattern_set = get_pattern_set()
+        self.x, self.y = 1000, 300
+        self.boss_hp = 800
+        self.hp = self.boss_hp
+        self.hp_bar = Boss_HP()
+
     def update(self, frame_time, events=None):
         pass
     def draw(self):
