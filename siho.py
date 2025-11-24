957e9fb97014269a6dfdfa1a939f780084a23fc9
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-24 23:53:59 +0900

    hit_timer 뼈대 완성

--- siho.py
+++ siho.py
@@ -86,3 +86,6 @@ class Boss_Siho:
         self.shape.y = self.y + self.height * 0.2
         self.hit_animation = False
         self.hit = True
+
+    def hit_timer(self, frame_time):
+        pass
