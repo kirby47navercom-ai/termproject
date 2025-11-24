cff8e4446a1dddacd4489c0e1acb3148c531c869
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-24 22:26:46 +0900

    임포트와 사이즈 넣기

---
+++ siho.py
@@ -0,0 +1,10 @@
+from boss_hp import Boss_HP
+from pattern import *
+from resource import *
+from random import randint
+import canvas_size
+import ramona
+import resource
+import math
+
+SIZE = 1.8
