4cfdd267fdd31545561cb35b4d10e41b64ff9975
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-25 20:35:35 +0900

    패턴 3 불도 계속 업데이트 되게

--- siho.py
+++ siho.py
@@ -87,6 +87,7 @@ class Boss_Siho:
 
         if self.pattern_num != 14:
             self.update_fireballs(frame_time)
+            self.update_pattern3_fireball(frame_time)
 
 
         self.shape.x = self.x - canvas_size.camera_x
