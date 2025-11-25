5b700cc39c852371330646104d003524c9a5b6ef
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-25 23:18:14 +0900

    ramonatoscratch3 완성

--- siho.py
+++ siho.py
@@ -552,7 +552,14 @@ class Boss_Siho:
                         continue
 
     def ramonatoscratch3(self,attack):
-
+        if collide([ramona.Ramona_POS_X, ramona.Ramona_POS_Y, ramona.Ramona_SIZE_X, ramona.Ramona_SIZE_Y],
+                   [attack[0], attack[1], 54 * 2,
+                    220 * 2]) and not ramona.Ramona_invincible and not ramona.Ramona_roll_invincible:
+            if ramona.CURRENT_HP > 0:
+                ramona.CURRENT_HP -= 1
+                ramona.Ramona_invincible = True
+                ramona.invincible_timer = 0.0
+                canvas_size.start_shake(0.5, 5.0)
 
 
     def draw(self):
