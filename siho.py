b7239bd1295cec10c192182f1b00b013719afd6b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-25 15:01:02 +0900

    pattern2 뼈대 설정

--- siho.py
+++ siho.py
@@ -96,6 +96,9 @@ class Boss_Siho:
             if self.hp <= 550:
                 self.pattern_num = 6
 
+    def pattern2(self, frame_time):
+        pass
+
 
     def draw(self):
         if not self.hit or (self.hit and (get_time() % 0.2) > 0.1):
