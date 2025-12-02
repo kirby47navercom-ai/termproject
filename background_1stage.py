8b7d5ffab22be90cda2367015d8cdaf004c4de6e
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-02 18:44:14 +0900

    배경 1 다듬기

--- background_1stage.py
+++ background_1stage.py
@@ -2,8 +2,10 @@ from pico2d import *
 import canvas_size
 import resource
 
+
 stage1width = 1980
 stage1height = 1080
+
 
 start=False
 
@@ -12,34 +14,26 @@ class Background:
     def __init__(self):
         resource.blocks.clear()
         if Background.background == None:
-            Background.background = [load_image('1stage\\1.png'), load_image('1stage\\2.png'),
-                                     load_image('1stage\\3.png'),
-                                     load_image('1stage\\4.png'), load_image('1stage\\5.png'),
-                                     load_image('1stage\\6.png'),
-                                     load_image('1stage\\7.png'), load_image('1stage\\8.png'), ]
-        self.x = [0, 0, 0, 0]
-        self.speed = 300
+            Background.background =[load_image('1stage\\1.png'), load_image('1stage\\2.png'),
+                               load_image('1stage\\3.png'),
+                               load_image('1stage\\4.png'), load_image('1stage\\5.png'),
+                               load_image('1stage\\6.png'),
+                               load_image('1stage\\7.png'), load_image('1stage\\8.png'), ]
+        self.x = [0,0,0,0]
+        self.speed=300
 
-    def update(self, frame_time, events=None):
+
+    def update(self, frame_time,events=None):
         for i in range(self.x.__len__()):
-            self.x[i] += self.speed * frame_time * (i + 1) * 0.2
+            self.x[i] += self.speed * frame_time * (i+1) * 0.2
             if self.x[i] >= stage1width * 0.7:
                 self.x[i] = 0
 
+
     def draw(self):
         for i in range(self.background.__len__()):
-            if i == 2 or i == 3 or i == 4:
-                self.background[i].clip_draw(0, 0, stage1width, stage1height,
-                                             canvas_size.canvaswidth // 2 + self.x[i - 2] - (
-                                                         stage1width * 0.7) - canvas_size.shake_x,
-                                             canvas_size.canvasheight // 2 - canvas_size.shake_y, stage1width * 0.7,
-                                             stage1height * 0.7)
-                self.background[i].clip_draw(0, 0, stage1width, stage1height,
-                                             canvas_size.canvaswidth // 2 + self.x[i - 2] - canvas_size.shake_x,
-                                             canvas_size.canvasheight // 2 - canvas_size.shake_y, stage1width * 0.7,
-                                             stage1height * 0.7)
+            if i==2 or i==3 or i==4:
+                self.background[i].clip_draw(0, 0, stage1width, stage1height, canvas_size.canvaswidth // 2 + self.x[i-2]-(stage1width * 0.7)-canvas_size.shake_x,canvas_size.canvasheight // 2-canvas_size.shake_y, stage1width * 0.7, stage1height * 0.7)
+                self.background[i].clip_draw(0, 0, stage1width, stage1height, canvas_size.canvaswidth//2+self.x[i-2]-canvas_size.shake_x,canvas_size.canvasheight//2 -canvas_size.shake_y, stage1width*0.7, stage1height*0.7)
             else:
-                self.background[i].clip_draw(0, 0, stage1width, stage1height,
-                                             canvas_size.canvaswidth // 2 - canvas_size.shake_x,
-                                             canvas_size.canvasheight // 2 - canvas_size.shake_y, stage1width * 0.7,
-                                             stage1height * 0.7)
+                self.background[i].clip_draw(0, 0, stage1width, stage1height, canvas_size.canvaswidth//2-canvas_size.shake_x,canvas_size.canvasheight//2-canvas_size.shake_y, stage1width*0.7, stage1height*0.7)
