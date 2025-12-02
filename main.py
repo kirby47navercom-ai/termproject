112062f7490f3de2e8ddb86b84ddef3e05312092
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-02 18:47:27 +0900

    마우스 화면안에 가두게 하고 시작을 다른 파일로 하기

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pico2d
+from sdl2 import SDL_SetRelativeMouseMode, SDL_TRUE
 import game_framework
-import game_manager as start_mode
+import logo_mode as start_mode
 import resource
 from canvas_size import *
 
 pico2d.open_canvas(canvaswidth, canvasheight)
 pico2d.hide_lattice()
 pico2d.hide_cursor()
+SDL_SetRelativeMouseMode(SDL_TRUE)
 resource.load_resources()
 game_framework.ingame(start_mode)
 pico2d.close_canvas()
