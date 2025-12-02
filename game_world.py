79b901991904fc186cbecdd9f725f4b3d6bae00e
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-02 18:46:40 +0900

    게임 월드 파일 넣음

---
+++ game_world.py
@@ -0,0 +1,31 @@
+world = [[] for _ in range(8)] # layers for game objects
+
+def add_object(o, depth):
+    world[depth].append(o)
+
+def add_objects(ol, depth):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
+
+
+def update(frame_time, events):
+    for layer in world:
+        for o in layer:
+            o.update(frame_time, events)
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+
+def clear():
+    for layer in world:
+        layer.clear()
