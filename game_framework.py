e48c4dac3993d8f3f8b37ec680c48f05ae0f99e7
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-02 18:45:49 +0900

    키에 따른 디버깅 만들기

--- game_framework.py
+++ game_framework.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 import time
 
+import canvas_size
+import mouse_image
+from pico2d import *
+from canvas_size import *
+import resource
+import ramona
 root = tk.Tk()
+
 
 def change_mode(mode):
     global stack
@@ -12,6 +19,7 @@ def change_mode(mode):
         stack.pop()
     stack.append(mode)
     mode.init()
+
 
 def push_mode(mode):
     global stack
@@ -39,18 +47,43 @@ def quit():
     running = False
 
 
+
+
 def ingame(start_mode):
     global running, stack
     running = True
     stack = [start_mode]
     start_mode.init()
-
+    mouse = mouse_image.Mouse()
     global frame_time
     frame_time = 0.0
     current_time = time.time()
     while running:
-        stack[-1].update(frame_time)
-        stack[-1].draw()
+        events = get_events()
+        mouse.update(frame_time,events)
+
+        for event in events:
+            if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_F1:
+                    canvas_size.collide_check = not canvas_size.collide_check
+                elif event.key == SDLK_F2:
+                    resource.coin = 9999
+                elif event.key == SDLK_F3:
+                    ramona.Ramona_attack=1000
+                elif event.key == SDLK_F4:
+                    resource.stage1_clear = 2
+                    resource.stage2_clear = 2
+                    resource.stage3_clear = 2
+                elif event.key == SDLK_ESCAPE:
+                    quit()
+
+
+        stack[-1].update(frame_time,events)
+        clear_canvas()
+        for mode in stack:
+            mode.draw()
+        mouse.draw()
+        update_canvas()
         frame_time = time.time() - current_time
         current_time += frame_time
 
