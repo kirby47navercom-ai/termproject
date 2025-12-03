598a8373ee6279e836e8c480879e8396bace6b8b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 23:07:02 +0900

    ending_mode init 완성

--- ending_mode.py
+++ ending_mode.py
@@ -8,3 +8,12 @@ black_background=None
 current_image = 0
 alpha = 0
 FADE_SPEED = 10  # 알파값 증가 속도
+
+def init():
+    global ending_image, current_image, alpha,black_background
+    if ending_image is None:
+        ending_image = resource.end_image
+        black_background = load_image('배경\\black_background.png')
+    current_image = 0
+    alpha = 0
+
