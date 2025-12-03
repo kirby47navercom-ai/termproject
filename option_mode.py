e7c70bffd016afee73d47d94ed2eedd7d43e7163
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 09:25:33 +0900

    option mode init 완성

--- option_mode.py
+++ option_mode.py
@@ -9,3 +9,11 @@ circle0 = []
 circle1 = []
 
 def init():
+    global option, circle0, circle1, black_background
+
+    if black_background == None:
+        option = load_image('배경\\option.png')
+        circle0 = [load_image(f'배경\\circle{str(i + 1)}.png') for i in range(5)]
+        circle1 = [load_image(f'배경\\circle{str(i + 6)}.png') for i in range(5)]
+        black_background = load_image('배경\\black_background.png')
+    pass
