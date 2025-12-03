d002c9ab9ed779e2514705cb99fe809b3fbab8a4
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 09:35:30 +0900

    na25 mode 사진 변수 선언

--- na25_mode.py
+++ na25_mode.py
@@ -4,3 +4,14 @@ import game_framework
 import resource
 import home_mode
 import ramona
+
+background= None
+nanahira = []
+inventory=None
+coin = None
+coin_font = None
+weapon1 = []
+weapon2 = []
+weapon3 = []
+weapon4 = []
+talk = []
