502acd2ffbbac1edc73deae0c5dee3f04778329c
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 20:08:39 +0900

    clear_mode 이미지 넣기

--- stage_clear_mode.py
+++ stage_clear_mode.py
@@ -10,4 +10,14 @@ clear = None
 food = []
 perfect = None
 
-def init():
+def init(): # 로고 이미지를 로드
+    global black_background,clear,food,perfect
+
+
+
+    if black_background == None:
+        black_background = load_image('배경\\black_background.png')
+        clear = load_image('배경\\clear.png')
+        food = [load_image('배경\\sugar.png'),load_image('배경\\water.png'),
+                load_image('배경\\lemon.png')]
+        perfect = load_image('배경\\perfect_no.png')
