6faf356ca88382a46734d01ee7a654174608c6c5
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 20:12:23 +0900

    fail_mode init 완성

--- stage_fail_mode.py
+++ stage_fail_mode.py
@@ -3,3 +3,24 @@ import canvas_size
 import game_framework
 import home_mode
 import resource
+
+
+black_background = None
+fail = None
+fail_no = None
+food = []
+
+
+def init():
+    # 로고 이미지를 로드
+    global black_background,fail,food,fail_no
+
+
+
+    if black_background == None:
+        black_background = load_image('배경\\black_background.png')
+        fail = load_image('배경\\fail.png')
+        food = [load_image('배경\\sugar.png'),load_image('배경\\water.png'),
+                load_image('배경\\lemon.png')]
+        fail_no = load_image('배경\\fail_no.png')
+
