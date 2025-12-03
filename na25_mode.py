57312596de86c58e0cbf20bad7fa165a368e9925
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 09:37:50 +0900

    na25 mode draw 완성

--- na25_mode.py
+++ na25_mode.py
@@ -140,3 +140,59 @@ def update(frame_time,events):
                 resource.talk = 0
 
 def draw():
+    background.draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.nanahira == 1:
+        nanahira[1].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    else:
+        nanahira[0].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    inventory.draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.weapon1 == 1:
+        weapon1.draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.weapon2 == 2:
+        weapon2[1].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.weapon2 == 1:
+        weapon2[0].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.weapon3 == 2:
+        weapon3[1].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.weapon3 == 1:
+        weapon3[0].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.weapon4 == 2:
+        weapon4[1].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.weapon4 == 1:
+        weapon4[0].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    if resource.talk == 1:
+        talk[0].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.talk == 2:
+        talk[1].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.talk == 3:
+        talk[2].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    elif resource.talk == 4:
+        talk[3].draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+
+    coin.draw(canvas_size.canvaswidth // 2, canvas_size.canvasheight // 2)
+    coin_font.draw(80, canvas_size.canvasheight - 40, 'X ' + str(resource.coin), (0, 0, 0))
+    if canvas_size.collide_check:
+        draw_rectangle(495, 605, 855, 670)  # 집
+
+        draw_rectangle(833, 540, 873, 580)  # 망치 1 ?
+
+        draw_rectangle(1060, 540, 1100, 580)  # 망치 2 ?
+
+        draw_rectangle(835, 245, 875, 285)  # 망치 3 ?
+
+        draw_rectangle(1060, 245, 1100, 285)  # 망치 4 ?
+
+        draw_rectangle(860, 318, 1010, 390)  # 망치 1 고르기
+
+        draw_rectangle(1090, 318, 1240, 390)  # 망치 2 고르기
+
+        draw_rectangle(860, 28, 1010, 100)  # 망치 3 고르기
+
+        draw_rectangle(1090, 28, 1240, 100)  # 망치 4 고르기
