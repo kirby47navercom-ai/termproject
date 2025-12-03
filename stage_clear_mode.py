33d231d662d2ab73fee38fcc08c47948c4c09029
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 20:09:00 +0900

    clear_mode 보스 1을 잡았을 경우

--- stage_clear_mode.py
+++ stage_clear_mode.py
@@ -21,3 +21,20 @@ def init(): # 로고 이미지를 로드
         food = [load_image('배경\\sugar.png'),load_image('배경\\water.png'),
                 load_image('배경\\lemon.png')]
         perfect = load_image('배경\\perfect_no.png')
+
+
+    if resource.boss1:
+        if ramona.MAX_HP != ramona.CURRENT_HP:
+            if resource.stage1_clear != 2:
+                resource.stage1_clear = 1
+            if resource.stage1_coin == 0:
+                resource.coin += 1
+                resource.stage1_coin=1
+        else:
+            resource.stage1_clear = 2
+            if resource.stage1_coin == 0:
+                resource.coin += 2
+                resource.stage1_coin = 2
+            elif resource.stage1_coin == 1:
+                resource.coin += 1
+                resource.stage1_coin = 2
