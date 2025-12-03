afce1a71547359e15e7285af964c034cf0f3ccf0
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 20:13:47 +0900

    ending_mode 이미지 변수 설정

--- ending_mode.py
+++ ending_mode.py
@@ -2,3 +2,9 @@ from pico2d import *
 import canvas_size
 import game_framework
 import resource
+
+ending_image = None
+black_background=None
+current_image = 0
+alpha = 0
+FADE_SPEED = 10  # 알파값 증가 속도
