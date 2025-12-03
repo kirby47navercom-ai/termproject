e951a8ad8ba77490e4c9c02e2ac22f7b0eec4b83
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-03 20:09:58 +0900

    clear_mode 홈모드로 돌아가기

--- stage_clear_mode.py
+++ stage_clear_mode.py
@@ -70,3 +70,16 @@ def init(): # 로고 이미지를 로드
                 resource.stage3_coin = 2
 
 def update(frame_time,events):
+    for event in events:
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, canvas_size.canvasheight - 1 - event.y
+            if 478 <= x <= 584 and 67 <= y <= 175:
+                if resource.boss1:
+                    resource.boss1 = False
+                elif resource.boss2:
+                    resource.boss2 = False
+                elif resource.boss3:
+                    resource.boss3 = False
+
+                game_framework.pop_mode()
+                game_framework.change_mode(home_mode)
