fc7ca264694e761c1680e7093961e71bd2086e64
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-07 21:17:03 +0900

    stage3 monster 첨 배경음 나오게 함

--- stage3_monster.py
+++ stage3_monster.py
@@ -1,24 +1,34 @@
 import draw_gesture
-import siho
 import stage3_topography
+import siho
 import ramona
+from random import randint
 import resource
 
 class Stage3_Monster:
     def __init__(self):
-        self.boss = siho.Boss_Siho()
+        self.phase_num=0
+        self.phase=[]
+        self.boss=siho.Boss_Siho()
         self.floor = stage3_topography.Stage3_Terrain()
+        resource.background_sound[8].set_volume(
+            (resource.background_sound_offset[8] * resource.bgm) // 2)
+        resource.background_sound[8].play(-1)
+
+
+
+
 
     def update(self, frame_time, events=None):
         self.boss.update(frame_time, events)
 
-        if self.boss.appear_animation and self.boss.pattern_num != 14:
+        if self.boss.appear_animation and self.boss.pattern_num!=14:
             self.floor.update(frame_time, events)
             self.shape_check()
 
             if self.boss.change_graphycs:
-                self.floor.current_pattern += 1
-                self.boss.change_graphycs = False
+                self.floor.current_pattern+=1
+                self.boss.change_graphycs=False
         self.monster_die()
 
     def shape_check(self):
@@ -35,7 +45,14 @@ class Stage3_Monster:
         if self.boss.die:
             resource.boss3 = True
 
+
+
+
     def draw(self):
         if self.boss.appear_animation:
             self.floor.draw()
         self.boss.draw()
+
+
+
+
