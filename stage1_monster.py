c9311dcbb7136bea7bb579b175315a3b13e9f2e1
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-07 17:33:27 +0900

    stage1_monster 전체적인 코드 수정

--- stage1_monster.py
+++ stage1_monster.py
@@ -8,37 +8,42 @@ import resource
 
 
 
-
 class Stage1_Monster:
     def __init__(self):
-        self.phase_num = 0
-        self.phase = []
+        self.phase_num=0
+        self.phase=[]
         self.phase.append(stage1_phase1.Stage1_Phase1())
         self.phase.append(stage1_phase2.Stage1_Phase2())
         self.phase.append(stage1_phase3.Stage1_Phase3())
         self.phase.append(stage1_phase4.Stage1_Phase4())
         self.phase.append(stage1_phase5.Stage1_Phase5())
 
+
         resource.background_sound[3].set_volume(
             (resource.background_sound_offset[3] * resource.bgm) // 2)
         resource.background_sound[3].play(-1)
 
-        self.bgm_change = False
+        self.bgm_change=False
 
 
     def update(self, frame_time, events=None):
-        self.phase[self.phase_num].update(frame_time, events)
 
-        if self.phase_num == 4 and not self.bgm_change:
+        self.phase[self.phase_num].update(frame_time,events)
+
+
+
+        if self.phase_num==4 and not self.bgm_change:
             resource.background_sound[3].stop()
             resource.background_sound[4].set_volume(
                 (resource.background_sound_offset[3] * resource.bgm) // 2)
             resource.background_sound[4].play(-1)
-            self.bgm_change = True
+            self.bgm_change=True
             pass
-        elif self.phase[self.phase_num].phase.__len__() == 0:
-            self.phase_num += 1
-            self.update(frame_time, events)
+        elif self.phase_num!=4 and self.phase[self.phase_num].phase.__len__()==0:
+            self.phase_num+=1
+            self.update(frame_time,events)
 
+        pass
     def draw(self):
         self.phase[self.phase_num].draw()
+        pass
