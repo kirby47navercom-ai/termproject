a8aced9b640cffec71ab513d729875049e9d1c33
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-07 17:23:21 +0900

    siho 패턴 2번 소리 넣음

--- siho.py
+++ siho.py
@@ -217,6 +217,10 @@ class Boss_Siho:
 
                 self.jump_frame = 0
 
+                resource.stage3_effect_sound[4].set_volume(
+                    (resource.stage3_effect_sound_offset[4] * resource.effect) // 2)
+                resource.stage3_effect_sound[4].play(1)
+
         elif self.pattern2_state == 1:
             self.x += self.v_x * frame_time
             self.y_velocity -= self.gravity * frame_time
