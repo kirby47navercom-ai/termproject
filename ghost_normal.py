7abea35193249021110b9554d859c1a3fb54ae40
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-07 11:58:04 +0900

    boss ghost  버그 수정

--- ghost_normal.py
+++ ghost_normal.py
@@ -1,4 +1,6 @@
+from pico2d import *
 
+import resource
 from pattern import *
 from resource import *
 from random import randint
@@ -6,32 +8,34 @@ import canvas_size
 import ramona
 import math
 
+
 class Ghost:
     image=None
     def __init__(self):
         self.pattern_set = get_pattern_set()
-        if Ghost.image == None:
-            Ghost.image = load_image('1stage\\level1-png-sprite.png')
+        if Ghost.image==None:
+            Ghost.image=load_image('1stage\\level1-png-sprite.png')
         self.x, self.y = -100, 0
-        self.hp = 20
+        self.hp=20
         self.width, self.height = 59, 76
         self.frame = 0
         self.dir = 1
         self.timer = 0.0
         self.speed = 50
 
-        self.shape = self.pattern_set[randint(0, pattern_number - 5)]
+        self.shape=self.pattern_set[randint(0,pattern_number-5)]
         self.shape.x = self.x
         self.shape.y = self.y + self.height * 0.7
 
-        self.die = False
-        self.die_animation = False
+
+        self.die=False
+        self.die_animation=False
         self.die_animation_speed = 8.0
-        self.die_frame = 0
-        self.hit = False
+        self.die_frame=0
+        self.hit=False
         self.hit_animation = False
         self.hit_animation_speed = 8.0
-        self.hit_frame = 0
+        self.hit_frame=0
 
     def update(self, frame_time, events=None):
 
@@ -45,6 +49,8 @@ class Ghost:
         self.ramonatoghost()
         self.die_ghost()
 
+        pass
+
     def move(self, frame_time):
         distance = math.sqrt((self.x - ramona.Ramona_POS_X) ** 2 + (self.y - ramona.Ramona_POS_Y) ** 2)
         self.x = self.x + (ramona.Ramona_POS_X - self.x) * self.speed * frame_time / distance
@@ -54,33 +60,35 @@ class Ghost:
         self.shape.y = self.y + self.height * 0.7
 
     def ramonatoghost(self):
-        if collide([ramona.Ramona_POS_X, ramona.Ramona_POS_Y, ramona.Ramona_SIZE_X, ramona.Ramona_SIZE_Y],
-                   [self.x, self.y, self.width,
-                    self.height]) and not ramona.Ramona_invincible and not ramona.Ramona_roll_invincible and not self.die_animation:
-            if ramona.CURRENT_HP > 0:
-                ramona.CURRENT_HP -= 1
-                ramona.Ramona_invincible = True
-                ramona.invincible_timer = 0.0
+        if collide([ramona.Ramona_POS_X,ramona.Ramona_POS_Y,ramona.Ramona_SIZE_X,ramona.Ramona_SIZE_Y],
+                   [self.x,self.y,self.width,self.height]) and not ramona.Ramona_invincible and not ramona.Ramona_roll_invincible and not self.die_animation:
+            if ramona.CURRENT_HP>0:
+                ramona.CURRENT_HP-=1
+                ramona.Ramona_invincible=True
+                ramona.invincible_timer=0.0
                 self.die_animation = True
                 canvas_size.start_shake(0.5, 5.0)
+
+
+        pass
 
     def hit_ghost_animation(self, frame_time):
         self.hit_frame = (self.hit_frame + self.hit_animation_speed * frame_time) % 4
         if int(self.hit_frame) == 3:
-            if self.hp > 0:
+            if self.hp>0:
                 resource.stage1_effect_sound[0].set_volume(
                     (resource.stage1_effect_sound_offset[0] * resource.effect) // 2)
                 resource.stage1_effect_sound[0].play(1)
             self.hit_animation = False
-            self.shape = self.pattern_set[randint(0, pattern_number - 5)]
+            self.shape = self.pattern_set[randint(0, pattern_number-5)]
             self.shape.x = self.x
             self.shape.y = self.y + self.height * 0.7
         pass
 
     def die_ghost(self):
-        if self.hp <= 0 and not self.die_animation:
-            self.die_animation = True
-            self.shape.name = 'No'
+        if self.hp<=0 and not self.die_animation:
+            self.die_animation=True
+            self.shape.name='No'
             resource.stage1_effect_sound[1].set_volume(
                 (resource.stage1_effect_sound_offset[1] * resource.effect) // 2)
             resource.stage1_effect_sound[1].play(1)
@@ -91,26 +99,22 @@ class Ghost:
         self.die_frame = (self.die_frame + self.die_animation_speed * frame_time) % 4
         if int(self.die_frame) == 3:
             self.die = True
-
-
-
+        pass
 
     def draw(self):
+
         if not self.die:
 
             if self.die_animation:
                 left, bottom, width, height, jx, jy = ghost_die_coordinate[int(self.die_frame)]
             elif self.hit_animation:
                 left, bottom, width, height, jx, jy = ghost_hit_coordinate[int(self.hit_frame)]
-            else:
-                left, bottom, width, height, jx, jy = ghost_idle_coordinate
+            else:  left, bottom, width, height, jx, jy = ghost_idle_coordinate
 
             if ramona.Ramona_POS_X < self.x:
-                self.image.clip_composite_draw(left, bottom, width, height, 0, '', self.x + jx - canvas_size.shake_x,
-                                               self.y + jy - canvas_size.shake_y, width, height)
+                self.image.clip_composite_draw(left, bottom, width, height, 0, '', self.x + jx-canvas_size.shake_x, self.y + jy-canvas_size.shake_y, width, height)
             else:
-                self.image.clip_composite_draw(left, bottom, width, height, 0, 'h', self.x + jx - canvas_size.shake_x,
-                                               self.y + jy - canvas_size.shake_y, width, height)
+                self.image.clip_composite_draw(left, bottom, width, height, 0, 'h', self.x + jx-canvas_size.shake_x, self.y + jy-canvas_size.shake_y, width, height)
 
             if canvas_size.collide_check:
                 draw_rectangle(self.x - width / 2,
