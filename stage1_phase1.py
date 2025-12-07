efcc5bf8c66442024c689a75860108597eabd4f0
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-12-07 19:27:21 +0900

    stage1_2p 약간 수정

--- stage1_phase1.py
+++ stage1_phase1.py
@@ -1,41 +1,49 @@
 from ghost_normal import *
 import draw_gesture
 
-
 ghost_phase_far=50
-ghost_phase_pos=[-1,-1,1,1,1]
+ghost_phase_pos=[-1,1,-1,1,-1,1,-1,1]
 
-class Stage1_Phase1:
+class Stage1_Phase2:
     def __init__(self):
-        self.phase=[Ghost() for _ in range(5)]
+        self.phase=[Ghost() for _ in range(ghost_phase_pos.__len__())]
         for i in range(self.phase.__len__()):
             self.phase[i].x=0 if ghost_phase_pos[i] == -1 else canvaswidth+ghost_phase_pos[i]*ghost_phase_far
             self.phase[i].y=random.randint(0,canvasheight)
+            self.phase[i].hp = random.randint(20,30)
 
-
+        self.num = 2
 
         pass
     def update(self, frame_time, events=None):
-        self.phase[self.phase.__len__()-1].update(frame_time,events)
+
+        for i in range(self.num):
+            self.phase[i].update(frame_time,events)
 
         self.shape_check()
         self.monster_die()
         pass
 
     def shape_check(self):
-        if self.phase[self.phase.__len__()-1].shape.name==draw_gesture.result:
-            self.phase[self.phase.__len__()-1].hp-=ramona.Ramona_attack
-            self.phase[self.phase.__len__()-1].hit_animation=True
-            self.phase[self.phase.__len__()-1].hit_frame = 0
-            ramona.Ramona_smash = True
+        for i in range(self.num):
+            if self.phase[i].shape.name==draw_gesture.result:
+                self.phase[i].hp-=ramona.Ramona_attack
+                self.phase[i].hit_animation=True
+                self.phase[i].hit_frame = 0
+                ramona.Ramona_smash = True
 
         draw_gesture.result=None
 
         pass
     def monster_die(self):
-        if self.phase[self.phase.__len__()-1].die:
-            self.phase.pop(self.phase.__len__()-1)
+        for i in range(self.num - 1, -1, -1):
+            if self.phase[i].die:
+                self.phase.pop(i)
+                self.num -= 1
+
+        if self.num == 0 and self.phase.__len__()>0: self.num = 2
         pass
     def draw(self):
-        self.phase[self.phase.__len__()-1].draw()
+        for i in range(self.num):
+            self.phase[i].draw()
         pass
